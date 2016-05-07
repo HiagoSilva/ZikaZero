f43da2684896678448b6b2044f7ce8c287b28887
Hiago Silva <hiago.as@hotmail.com>
2016-05-07 12:41:18 -0400

    Alteração tela de login (mudanca de pasta para Dropbox)

--- app/src/main/java/com/projeto/zikazero/LoginActivity.java
+++ app/src/main/java/com/projeto/zikazero/LoginActivity.java
@@ -2,12 +2,26 @@ package com.projeto.zikazero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsuario;
+    private EditText etSenha;
+    private TextView tvEsqueciSenha;
+    private Button bLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsuario       = (EditText) findViewById(R.id.et_usuario);
+        etSenha         = (EditText) findViewById(R.id.et_senha);
+        tvEsqueciSenha  = (TextView) findViewById(R.id.tv_esqueci_minha_senha);
+        bLogar          = (Button) findViewById(R.id.b_logar);
+
     }
 }
