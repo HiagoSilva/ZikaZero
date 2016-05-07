5fb236ab974a9e22458a9fde224c232afab36e3c
Hiago Silva <hiago.as@hotmail.com>
2016-05-07 12:10:24 -0400

    Novo Foco, chamado da tela main, tirando foto e colocando no ImageView

--- app/src/main/java/com/projeto/zikazero/MainActivity.java
+++ app/src/main/java/com/projeto/zikazero/MainActivity.java
@@ -1,18 +1,42 @@
 package com.projeto.zikazero;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    ImageView img;
-    private Bitmap bitmap;
+    private Button btNovoFoco;
+    private Button btMapaFocos;
+    private Button btPosts;
+    private Button btEstatistica;
+    private Button btInformacoes;
+    private Button btNoticias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btNovoFoco      = (Button) findViewById(R.id.b_novo_foco);
+        btMapaFocos     = (Button) findViewById(R.id.b_mapa_focos);
+        btPosts         = (Button) findViewById(R.id.b_posts);
+        btEstatistica   = (Button) findViewById(R.id.b_estatistica);
+        btInformacoes   = (Button) findViewById(R.id.b_informaoes);
+        btNoticias      = (Button) findViewById(R.id.b_noticias);
+
+        btNovoFoco.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, NovoFocoActivity.class);
+                startActivity(it);
+            }
+        });
     }
+
+
+
 }
--- app/src/main/java/com/projeto/zikazero/NovoFocoActivity.java
+++ app/src/main/java/com/projeto/zikazero/NovoFocoActivity.java
@@ -1,13 +1,46 @@
 package com.projeto.zikazero;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class NovoFocoActivity extends AppCompatActivity {
+
+    private ImageView imgFoco;
+    private EditText descricaoLugar;
+    private TextView localizacaoAuto;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_foco);
+
+        imgFoco = (ImageView) findViewById(R.id.iv_foto);
+        descricaoLugar = (EditText) findViewById(R.id.et_descricao);
+        localizacaoAuto = (TextView) findViewById(R.id.tv_localizacao_auto);
+    }
+
+    public void tirarFoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgFoco.setImageBitmap(imageBitmap);
+        }
     }
 }
